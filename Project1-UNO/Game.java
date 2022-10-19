d065c7c771c457162c4f1e0d1fed9e3cc7147085
tristentrn <tristen.tran@yahoo.com>
2022-10-19 11:45:24 -0700

    finished player class

--- Project1-UNO/Game.java
+++ Project1-UNO/Game.java
@@ -8,13 +8,24 @@
  */
 public class Game
 {
+	
+	//gameDirection
+	//set number of card to players hand
+	//get information about players and store it
+	//set Card color
+	//StartGame(get player inforamation, set number of card player has, direction of player)
 
 	/**
 	 * @param args
 	 */
-	public void main(String[] args)
+	public static void main(String[] args)
 	{
-	Card.createCard();
+	Player player = new Player(0);
+	player.getPlayerName();
+	
+//	//public String[] getPlayers() {
+//		return players;
+//	}
 	
 
 	}
