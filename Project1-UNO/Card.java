339a5619b90c1d5ca71d53103099f607cc12739b
Sean- <Sean-@PC-SeanFuller.lan>
2022-10-23 14:41:09 -0700

    2022-10-23 14:41

--- Project1-UNO/Card.java
+++ Project1-UNO/Card.java
@@ -6,13 +6,13 @@ import java.util.*;
 
 public class Card {
 	// Card Variables
-	int cardColor; // 0: Red, 1: Blue, 2: Yellow, 3: Green, 4: Black (Draw Four & Wild Card only)
-	int cardType; // 0 - 8: Number Card (1-9), 9: Skip Card, 10: Reverse Card, 11: Draw Four Card, 12: Wild Card
+	int cardColor; // 0: Red, 1: Blue, 2: Yellow, 3: Green, 4: Black (Draw Four Card only)
+	int cardType; // 0-9: Number Card (0-9), 10: Skip Card, 11: Reverse Card, 12: Draw Four Card
 	
 	/*
 	 *Card Type Explanations:
 	 *
-	 *Number Card (1-9): A basic card that can be played if its number OR color matches that of the card on the field.
+	 *Number Card (0-9): A basic card that can be played if its number OR color matches that of the card on the field.
 	 *
 	 *Skip Card:		 An action card that can be played if its color matches that of the card on the field.
 	 * 			 		 Upon playing this card, it will skip the next player's turn.
@@ -22,9 +22,6 @@ public class Card {
 	 *
 	 *Draw Four Card: 	 An action card that can be played regardless of the number or color of the card on the field.
 	 *				  	 Upon playing this card, the next player must draw four cards into their deck.
-	 *
-	 *Wild Card: 		 An action card that can be played regardless of the number or color of the card on the field.
-	 *			 		 Upon playing this card, the player may choose its number AND color.
 	*/
 	
 	// Card Constructor
@@ -37,8 +34,8 @@ public class Card {
 	public Card createCard() {
 		Card newCard = new Card();
 		Random random = new Random();
-		cardType = random.nextInt(12);
-		if (cardType == 11) {
+		cardType = random.nextInt(13);
+		if (cardType == 12) {
 			cardColor = 4;
 		}
 		else {
@@ -75,74 +72,47 @@ public class Card {
 		String cardTypeString = null;
 		
 		if (this.cardType == 0) {
-			cardTypeString = "1";
+			cardTypeString = "0";
 		}
 		else if (this.cardType == 1) {
-			cardTypeString = "2";
+			cardTypeString = "1";
 		}
 		else if (this.cardType == 2) {
-			cardTypeString = "3";
+			cardTypeString = "2";
 		}
 		else if (this.cardType == 3) {
-			cardTypeString = "4";
+			cardTypeString = "3";
 		}
 		else if (this.cardType == 4) {
-			cardTypeString = "5";
+			cardTypeString = "4";
 		}
 		else if (this.cardType == 5) {
-			cardTypeString = "6";
+			cardTypeString = "5";
 		}
 		else if (this.cardType == 6) {
-			cardTypeString = "7";
+			cardTypeString = "6";
 		}
 		else if (this.cardType == 7) {
-			cardTypeString = "8";
+			cardTypeString = "7";
 		}
 		else if (this.cardType == 8) {
-			cardTypeString = "9";
+			cardTypeString = "8";
 		}
 		else if (this.cardType == 9) {
-			cardTypeString = "Skip";
+			cardTypeString = "9";
 		}
 		else if (this.cardType == 10) {
-			cardTypeString = "Reverse";
+			cardTypeString = "Skip";
 		}
 		else if (this.cardType == 11) {
+			cardTypeString = "Reverse";
+		}
+		else if (this.cardType == 12) {
 			cardTypeString = "Draw Four";
 		}
-		
+	
 		return cardTypeString;
 	}
-	
-//	public Card changeWildCard(Card card) {
-//		Scanner scnr = new Scanner(System.in);
-//		System.out.println("Please enter a color to set the Wild Card to (0: Red, 1: Blue, 2: Yellow, 3: Green):");
-//		do {
-//		if (scnr.nextInt() == 0) {
-//			this.cardColor = 0;
-//			System.out.println("The Wild Card's color has been set to Red.");
-//			}
-//		else if (scnr.nextInt() == 1) {
-//			this.cardColor = 1;
-//			System.out.println("The Wild Card's color has been set to Blue.");
-//			}	
-//		else if (scnr.nextInt() == 2) {
-//			this.cardColor = 2;
-//			System.out.println("The Wild Card's color has been set to Yellow.");
-//			}
-//		else if (scnr.nextInt() == 3) {
-//			this.cardColor = 3;
-//			System.out.println("The Wild Card's color has been set to Green.");
-//			}
-//		} 
-//		catch (InputMismatchException e) {
-//			System.out.println("Invalid Input: To select a color, you must input the integer that corresponds to it.");
-//			scnr.nextLine();
-//			}
-//		
-//		scnr.close();
-//		return card;
-//	}
 	public static void main(String[] args) {
 	}
 }
